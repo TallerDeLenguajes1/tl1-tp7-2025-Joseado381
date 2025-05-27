3b803bbe31cab247e20214b20213d028fb9735df
Joseado381 <josealonzo.jaa@gmail.com>
2025-05-27 20:48:38 -0300

    commit sin descripcion

---
+++ Calculadora/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using EspacioCalculadora;
+
+Claculadora miCalculadora = new Claculadora();
+miCalculadora.Sumar(23);
+miCalculadora.Sumar(10);
+miCalculadora.Sumar(50);
+miCalculadora.dividir(0);
+miCalculadora.dividir(2);
+
+Console.WriteLine($"El resultado es: {miCalculadora.ObtenerResultado()}");
