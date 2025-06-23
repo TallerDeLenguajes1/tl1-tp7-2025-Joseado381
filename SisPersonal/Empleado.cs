387b2f4274f6e9060791b8b30e8c905c677abff3
Joseado381 <josealonzo.jaa@gmail.com>
2025-06-22 21:12:27 -0300

    otro commit sin nombre por que me canse de ponerles nombres y descripciones

---
+++ SisPersonal/Empleado.cs
@@ -0,0 +1,12 @@
+namespace SisEmpleados;
+
+public class Empleado
+{
+    private string Nombre;
+    private string Apellido;
+    private DateTime FechaNacimiento;
+    private char estadoCivil;
+    
+
+    
+}
