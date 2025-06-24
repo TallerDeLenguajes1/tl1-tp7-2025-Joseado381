61ef3e5e4a240ae173a1af54e5b5d7d9dad29cf2
Joseado381 <josealonzo.jaa@gmail.com>
2025-06-24 13:52:37 -0300

    Sistema de Empleados completado... creo

--- SisPersonal/Empleado.cs
+++ SisPersonal/Empleado.cs
@@ -1,108 +1,118 @@
-namespace SisEmpleados;
-
-public enum Cargos
+namespace SisPersonal
 {
-    Auxiliar = 0,
-    Administrativo = 1,
-    Ingeniero = 2,
-    Especialista = 3,
-    Investigador = 4,
-}
-public class Empleado
-{
-    private string nombre;
-    private string apellido;
-    private DateTime fechaNacimiento;
-    private char estadoCivil;
-    private DateTime fechaIngreso;
-    private double sueldoBasico;
-    private Cargos cargo;
-
-    public string Nombre { get => nombre; set => nombre = value; }
-    public string Apellido { get => apellido; set => apellido = value; }
-    public DateTime FechaNacimiento { get => fechaNacimiento; set => fechaNacimiento = value; }
-    public char EstadoCivil { get => estadoCivil; set => estadoCivil = value; }
-    public DateTime FechaIngreso { get => fechaIngreso; set => fechaIngreso = value; }
-    public double SueldoBasico { get => sueldoBasico; set => sueldoBasico = value; }
-    public Cargos Cargo { get => cargo; set => cargo = value; }
-
-    //constructor es el que inicializa los atributos de la clase
-    public Empleado(string nombre, string apellido, DateTime fechaNacimiento, char estadoCivil,
-    DateTime fechaIngreso, double sueldoBasico, Cargos cargo)
+    public enum Cargos {Auxiliar,Administrativo,Ingeniero,Especialista,Investigador}
+    public class Empleado
     {
-        this.nombre = nombre;
-        this.apellido = apellido;
-        this.fechaNacimiento = fechaNacimiento;
-        this.estadoCivil = estadoCivil;
-        this.fechaIngreso = fechaIngreso;
-        this.sueldoBasico = sueldoBasico;
-        this.cargo = cargo;
+        private string nombre;
+        private string apellido;
+        private DateTime fechaNacimiento;
+        private char estadoCivil;
+        private DateTime fechaIngreso;
+        private double sueldoBasico;
+        private Cargos cargo;
+
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Apellido { get => apellido; set => apellido = value; }
+        public DateTime FechaNacimiento { get => fechaNacimiento; set => fechaNacimiento = value; }
+        public char EstadoCivil { get => estadoCivil; set => estadoCivil = value; }
+        public DateTime FechaIngreso { get => fechaIngreso; set => fechaIngreso = value; }
+        public double SueldoBasico { get => sueldoBasico; set => sueldoBasico = value; }
+        public Cargos Cargo { get => cargo; set => cargo = value; }
+
+        //constructor es el que inicializa los atributos de la clase
+        public Empleado(string nombre, string apellido, DateTime fechaNacimiento, char estadoCivil,
+        DateTime fechaIngreso, double sueldoBasico, Cargos cargo)
+        {
+            this.nombre = nombre;
+            this.apellido = apellido;
+            this.fechaNacimiento = fechaNacimiento;
+            this.estadoCivil = estadoCivil;
+            this.fechaIngreso = fechaIngreso;
+            this.sueldoBasico = sueldoBasico;
+            this.cargo = cargo;
+        }
+
+        //método para calcular la antigüedad del empleado
+        public int CalcularAntiguedad()
+        {
+            DateTime fechaActual = DateTime.Now;
+            int antiguedad = fechaActual.Year - fechaIngreso.Year;
+
+            // Verificar si el empleado aún no ha cumplido años en el año actual
+            if (fechaActual < fechaIngreso.AddYears(antiguedad))
+            {
+                antiguedad--;
+            }
+
+            return antiguedad;
+        }
+
+        //calcularEdad
+        public int CalcularEdad()
+        {
+            DateTime fechaActual = DateTime.Now;
+            int edad = fechaActual.Year - fechaNacimiento.Year;
+
+            // Verificar si el empleado aún no ha cumplido años en el año actual
+            if (fechaActual < fechaNacimiento.AddYears(edad))
+            {
+                edad--;
+            }
+
+            return edad;
+        }
+
+        // años que faltan para la jubilación
+        public int CalcularJubilacion()
+        {
+            int edad = CalcularEdad();
+            int Tiempojubilacion = 65 - edad;
+            if (Tiempojubilacion < 0)
+            {
+                Tiempojubilacion = 0; // Si ya tiene más de 65 años, no faltan años para jubilarse
+            }
+            return Tiempojubilacion;
+        }
+
+        //método para calcular el sueldo total del empleado
+        public double CalcularSalario()
+        {
+            double adicional = 0;
+
+            if (CalcularAntiguedad() <= 20)
+            {
+                adicional = sueldoBasico * CalcularAntiguedad() * 0.01;
+            }
+            else
+            {
+                adicional = sueldoBasico * 0.25;
+            }
+            if (cargo == Cargos.Ingeniero || cargo == Cargos.Especialista)
+            {
+                adicional *= 1.50;
+            }
+            if (char.ToUpper(estadoCivil) == 'C')
+            {
+                adicional += 150000;
+            }
+
+            return (double)(sueldoBasico + adicional);
+        }
+
+        //Mostrar info Empleados
+        public string MostrarEmpleado()
+        {
+            return $"Nombre: {nombre} {apellido}\n" +
+                   $"Fecha de Nacimiento: {fechaNacimiento}\n" +
+                   $"Estado Civil: {estadoCivil}\n" +
+                   $"Fecha de Ingreso: {fechaIngreso}\n" +
+                   $"Sueldo Básico: {sueldoBasico}\n" +
+                   $"Cargo: {cargo}\n" +
+                   $"Antigüedad: {CalcularAntiguedad()} años\n" +
+                   $"Edad: {CalcularEdad()} años\n" +
+                   $"Años para Jubilación: {CalcularJubilacion()} años\n" +
+                   $"Sueldo Total: {CalcularSalario()}";
+        }
+
     }
-
-    //método para calcular la antigüedad del empleado
-    public int CalcularAntiguedad()
-    {
-        DateTime fechaActual = DateTime.Now;
-        int antiguedad = fechaActual.Year - fechaIngreso.Year;
-
-        // Verificar si el empleado aún no ha cumplido años en el año actual
-        if (fechaActual < fechaIngreso.AddYears(antiguedad))
-        {
-            antiguedad--;
-        }
-
-        return antiguedad;
-    }
-
-    //calcularEdad
-    public int CalcularEdad()
-    {
-        DateTime fechaActual = DateTime.Now;
-        int edad = fechaActual.Year - fechaNacimiento.Year;
-
-        // Verificar si el empleado aún no ha cumplido años en el año actual
-        if (fechaActual < fechaNacimiento.AddYears(edad))
-        {
-            edad--;
-        }
-
-        return edad;
-    }
-
-    // años que faltan para la jubilación
-    public int CalcularJubilacion()
-    {
-        int edad = CalcularEdad();
-        int Tiempojubilacion = 65 - edad;
-        if (Tiempojubilacion < 0)
-        {
-            Tiempojubilacion = 0; // Si ya tiene más de 65 años, no faltan años para jubilarse
-        }
-        return Tiempojubilacion;
-    }
-
-    //método para calcular el sueldo total del empleado
-    public double CalcularSalario()
-    {
-        double adicional = 0;
-
-        if (CalcularAntiguedad() <= 20)
-        {
-            adicional += sueldoBasico * CalcularAntiguedad() * 0.01;
-        }else
-        {
-            adicional += sueldoBasico * 0.25;
-        }
-        if (cargo == Cargos.Ingeniero || cargo == Cargos.Especialista)
-        {
-            adicional *= 1.5;
-        }
-        if (char.ToUpper(estadoCivil) == 'C')
-        {
-            adicional += 150000;
-        }
-        
-        return (double)(sueldoBasico + adicional);
-    }
-
 }
--- SisPersonal/Program.cs
+++ SisPersonal/Program.cs
@@ -1,5 +1,43 @@
 ﻿
-using SisEmpleados;
+using SisPersonal;
 
-Empleado empleado = new Empleado("Juan", "Perez", new DateTime(1990, 5, 15), 'S',
-new DateTime(2015, 3, 1), 1500.00, Cargos.Ingeniero);
+Empleado Jose = new Empleado
+("Jose","Alonzo",new DateTime(2003,04,24),'c',new DateTime(2010,03,20),650000,Cargos.Especialista);
+
+System.Console.WriteLine("Datos del Empleado:");
+System.Console.WriteLine(Jose.MostrarEmpleado());
+
+Empleado[] empleados = new Empleado[3];
+
+empleados[0] = new Empleado("Ana", "García", new DateTime(1990, 5, 12), 's', new DateTime(2015, 6, 1), 500000, Cargos.Administrativo);
+empleados[1] = new Empleado("Luis", "Pérez", new DateTime(1985, 8, 23), 'c', new DateTime(2010, 3, 20), 700000, Cargos.Especialista);
+empleados[2] = new Empleado("María", "López", new DateTime(1995, 2, 17), 's', new DateTime(2020, 1, 10), 450000, Cargos.Auxiliar);
+
+foreach (var empleado in empleados)
+{
+    System.Console.WriteLine("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+    System.Console.WriteLine($"Empleado: {empleado.Apellido} {empleado.Nombre}");
+    System.Console.WriteLine($"Antiguedad: {empleado.CalcularAntiguedad()} años");
+    System.Console.WriteLine($"Salario Total: {empleado.CalcularSalario()}");
+}
+System.Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+
+// Monto total de los salarios
+double SalarioTotal = 0;
+foreach (var empleado in empleados)
+{
+    SalarioTotal += empleado.CalcularSalario();
+}
+System.Console.WriteLine($"El salario gastado en empleados es de ${SalarioTotal}");
+
+int jubilarse = 65, i = 0;
+foreach (var empleado in empleados)
+{
+    if (empleado.CalcularJubilacion() < jubilarse)
+    {
+        jubilarse = empleado.CalcularJubilacion();
+        i++;
+    }
+}
+
+System.Console.WriteLine($"\nEl empleado mas proximo a jubilarse es:\n{empleados[i-1].MostrarEmpleado()}");
